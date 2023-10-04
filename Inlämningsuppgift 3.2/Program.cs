db1e4d8697ff49eb00912cb55fd828fb957cc2b8
ErnstOsbeck <143172129+ErnstOsbeck@users.noreply.github.com>
2023-10-04 10:44:05 +0200

    Add project files.

---
+++ Inlämningsuppgift 3.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace uppgift_3._2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Har du gått ut gymnasiet? (svara med \"j\" för ja och \"n\" nej)");
+            string svar = Console.ReadLine();
+            Console.WriteLine("Hur gammal är du?");
+            int ålder = int.Parse(Console.ReadLine());
+            if (svar == "j" && ålder < 22)
+            {
+                Console.WriteLine("Vi vill gärna anställa dig!");
+            }
+            else
+            {
+                Console.WriteLine("Vi letar tyvärr efter annan personal just nu.");
+            }
+            Console.Read();
+        }
+    }
+}
